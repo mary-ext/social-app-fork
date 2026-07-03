f217b0a0865665a6f5a13c7e17c6cda06384665b
Mary <git@mary.my.id>
2026-07-03 10:38:16 +0700

    refactor: port route-loading-screen to web

--- src/view/shell/route-loading-screen.tsx
+++ src/view/shell/route-loading-screen.tsx
@@ -1,18 +1,12 @@
-import { View } from 'react-native';
-
-import { atoms as a } from '#/alf';
-
-import * as Layout from '#/components/Layout';
-import { Spinner } from '#/components/Spinner';
+import { CenteredSpinner } from '#/components/CenteredSpinner';
+import * as Layout from '#/components/web/Layout';
 
 import { m } from '#/paraglide/messages';
 
 export function RouteLoadingScreen() {
 	return (
-		<Layout.Screen testID="routeLoadingScreen">
-			<View style={[a.flex_1, a.align_center, a.justify_center, a.p_lg]}>
-				<Spinner color="default" label={m['common.status.loading']()} size="2xl" />
-			</View>
+		<Layout.Screen>
+			<CenteredSpinner label={m['common.status.loading']()} size="2xl" fill />
 		</Layout.Screen>
 	);
 }
