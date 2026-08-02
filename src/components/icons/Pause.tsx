48d9d137dfe33488a12b597b3564c8d152862a97
Mary <git@mary.my.id>
2026-08-02 18:16:11 +0700

    refactor!: drop the tsx icon components in favour of svg sprites

--- src/components/icons/Pause.tsx
+++
@@ -1,5 +0,0 @@
-import { createSinglePathSVG } from './TEMPLATE';
-
-export const Pause_Filled_Corner0_Rounded = createSinglePathSVG({
-	path: 'M4 4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V4ZM14 4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1V4Z',
-});
