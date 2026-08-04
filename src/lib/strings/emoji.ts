db13d67d45f6808b6873c21867ef5dcbce09fac6
Mary <git@mary.my.id>
2026-08-04 14:55:54 +0700

    fix: ignore whitespaces when checking if it's only emojis

--- src/lib/strings/emoji.ts
+++ src/lib/strings/emoji.ts
@@ -1,5 +1,6 @@
 const SINGLE_EMOJI_RE = /^[\p{Emoji_Presentation}\p{Extended_Pictographic}\uFE0F\u200D]+$/u;
 
 export function isOnlyEmoji(text: string) {
-	return text.length <= 15 && SINGLE_EMOJI_RE.test(text);
+	const emoji = text.replace(/\s+/gu, '');
+	return emoji.length !== 0 && emoji.length <= 15 && SINGLE_EMOJI_RE.test(emoji);
 }
